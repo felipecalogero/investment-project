2a6bca05988fff13147031e1bf05a365d1e59e2d
felipecalogero <felipe@performmaonline.com>
2020-04-04 13:59:22 -0300

    Created a new function to get user authentication data and added new image in view login

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -6,6 +6,9 @@
         <link href="https://fonts.googleapis.com/css?family=Fredoka+One" rel="stylesheet">
     </head>
     <body>
+
+        <div class="background"></div>
+
         <section id="conteudo-view" class="login">
             <h1>Investindo</h1>
             <h3> O nosso gerenciador de investimento</h3>
